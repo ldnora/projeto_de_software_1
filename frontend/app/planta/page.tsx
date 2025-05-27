23369ce137c235674e69b9b3b6e87ea4d143da7d
Leandro Dalla Nora <workdallanora@gmail.com>
2025-05-27 16:29:32 -0300

    Fix: correção de um título da página

--- frontend/app/planta/page.tsx
+++ frontend/app/planta/page.tsx
@@ -85,7 +85,7 @@ export default async function Plantas() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-8">Our Team</h1>
+      <h1 className="text-3xl font-bold mb-8">Acervo</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {plantas.data.map((member: PlantaProps) => (
           <PlantaCard key={member.documentId} {...member} />
