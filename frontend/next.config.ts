e4bfe7edf5a2b5bb7b43a88be6da6faf343381de
Leandro Dalla Nora <workdallanora@gmail.com>
2025-06-24 16:21:02 -0300

    Chore: Removi algumas bibliotecas não utilizadas

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
-import withMDX from "@next/mdx";
-
-// Habilita o suporte a arquivos .mdx
-const withMdx = withMDX({
-  extension: /\.mdx?$/,
-});
 
 const nextConfig: NextConfig = {
-  // Disable eslinting during build
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -21,8 +14,5 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // Adiciona suporte a arquivos .mdx na pasta /app, /pages e /components
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
-
-export default withMdx(nextConfig);
